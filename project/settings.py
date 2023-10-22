6c6abe5aa64e1f98c3870daef0c56837c6399f4f
Shymaa Medhat <137145389+Shymaa2611@users.noreply.github.com>
2023-10-22 19:04:36 +0300

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -109,6 +109,7 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 TEMPLATES=[
     {
@@ -138,6 +139,5 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT=os.path.join(BASE_DIR,"media")
 MEDIA_URL = "media/"
-    
-AUTH_USER_MODEL = 'questions.User'
+
 

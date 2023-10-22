c6c79f6aaf1fcd4747d0e6de7ab159b3d38124e7
Shymaa Medhat <137145389+Shymaa2611@users.noreply.github.com>
2023-10-22 19:01:23 +0300

    Update forms.py

--- questions/forms.py
+++ questions/forms.py
@@ -1,25 +1,24 @@
 from django import forms
 from .models import User,Profile,Chapter
 
+from django.contrib.auth import authenticate
 
 class authenticationForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
-
     class Meta:
         model = User
         fields = ['username','email', 'password']
 
-
 class LoginForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
-
     class Meta:
-        model = User
-        fields = ['email', 'password']
+        model=User
+        fields=['username','password']
+
 
 
 class qusetionForm(forms.ModelForm):
     class Meta:
         model=Chapter
         fields=('chapter',)
- 
+

6de9e3a591d51dac25cf169fbacdbd120f232fcc
Shymaa Medhat <137145389+Shymaa2611@users.noreply.github.com>
2023-10-22 19:02:10 +0300

    Update models.py

--- questions/models.py
+++ questions/models.py
@@ -1,29 +1,21 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
-from .manager import CustomUserManager
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.contrib.auth.models import User
 
-class User(AbstractBaseUser, PermissionsMixin):
-    objects = CustomUserManager()
-    username=models.CharField(max_length=20)
-    email = models.EmailField(unique=True) 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
-    is_staff = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.email
-    
 class Chapter(models.Model):
     chapter=models.FileField(upload_to='chapters/',blank=True,null=True)
-    final_degree=models.IntegerField(blank=True,null=True)
+    #answer=models.BooleanField(default=False)
+class Question(models.Model):
+    chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE)
+    question=models.CharField(max_length=100,blank=True,null=True)
+    answer = models.CharField(max_length=100)
 
 
 class Profile(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
     chapter=models.ForeignKey(Chapter,on_delete=models.CASCADE,blank=True,null=True)
-    std_degree=models.DecimalField(max_digits=5,decimal_places=3,blank=True,null=True)
+    std_degree=models.IntegerField(blank=True,null=True)
     def __str__(self):
         return f"{self.user.email} Profile"
 
@@ -32,5 +24,13 @@ def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
+
+class UserResponse(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE) 
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    response = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.user.username}'s response to {self.question.content}"
 
     
